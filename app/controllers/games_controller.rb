20af13b2064b83a9fa546c9f8a7c186940f3204e
Coralie Loheac <coralie.loheac@gmail.com>
2019-11-11 14:46:02 +0100

    ex3

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { ('a'..'z').to_a.sample }
+  end
+
+  def score
+
+    @score = params[:word]
+    @letters = params[:letters]
+    @english_word = english_word?(params[:word])
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
